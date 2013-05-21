497463cae46288bd7fcedb6391109526e22d30ea
Eric Chesbrough <EricChesbrough@gmail.com>
2013-05-21 11:24:47 -0500

    new app for rpsec purposes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReviewApp::Application.config.secret_token = '28d72b529c78def6cb698aefbed8c83cd986f263ccf66a7b9ae2118f1556de916edf47d2152e72a20aac5440289529411d7f8ed652ecb00a12b285618e2cbe24'
